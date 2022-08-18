60c8fdcb962d173dbe13582f9e53ef404da5e71b
hpipou <mr.aoudjit@gmail.com>
2022-08-18 09:00:43 +0200

    Authentification login - register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./authentification/authentification.module').then(m => m.AuthentificationModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
